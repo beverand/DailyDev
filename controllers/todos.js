3b0177ebeae04e8757bd8982f9b1c9ed08b22a63
beverand <beverly.andrews@gmail.com>
2022-10-21 12:08:00 -0700

    remove commented out code for markComp/Incomp

--- controllers/todos.js
+++ controllers/todos.js
@@ -31,10 +31,8 @@ module.exports = {
     },
     createTodo: async (req, res)=>{
         try{
-            // await Todo.create({
-            //     todo: req.body.todoItem, completed: false, userId: req.user.id,
-            // })
-            
+           const n = await User.find({_id:req.user._id}).select('questions')
+
             let newQ = await Question.create({
                 question: req.body.todoItem, 
                 response: req.body.newAns,
@@ -56,28 +54,6 @@ module.exports = {
             console.log(err)
         }
     },
-    // markComplete: async (req, res)=>{
-    //     try{
-    //         await Todo.findOneAndUpdate({_id:req.body.todoIdFromJSFile},{
-    //             completed: true
-    //         })
-    //         console.log('Marked Complete')
-    //         res.json('Marked Complete')
-    //     }catch(err){
-    //         console.log(err)
-    //     }
-    // },
-    // markIncomplete: async (req, res)=>{
-    //     try{
-    //         await Todo.findOneAndUpdate({_id:req.body.todoIdFromJSFile},{
-    //             completed: false
-    //         })
-    //         console.log('Marked Incomplete')
-    //         res.json('Marked Incomplete')
-    //     }catch(err){
-    //         console.log(err)
-    //     }
-    // },
     deleteTodo: async (req, res)=>{
         console.log('deltodo',req.body.todoIdFromJSFile)
         const todoall = req.body
