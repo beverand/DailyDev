ae25de3d2b4a1735e1e1426241e034b79e3e580e
beverand <beverly.andrews@gmail.com>
2022-11-07 22:06:41 -0800

    added reference to profile in todo.js route

--- routes/todos.js
+++ routes/todos.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const todosController = require('../controllers/todos') 
+const profileController = require('../controllers/profile') 
 const { ensureAuth } = require('../middleware/auth')
 
 router.get('/', ensureAuth, todosController.getTodos)
+
+router.get('/getProfile', profileController.getProfile)
 
 router.post('/createTodo', todosController.createTodo)
 
